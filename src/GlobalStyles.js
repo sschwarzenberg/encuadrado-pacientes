b2627bc410ecf18815ab619a9c69b44f62733ae0
sschwarzenberg <sschwarzenberg@uc.cl>
2023-12-10 22:54:37 -0300

    Add styles

---
+++ src/GlobalStyles.js
@@ -0,0 +1,14 @@
+// GlobalStyles.js
+import { css } from '@emotion/react';
+
+const GlobalStyles = css`
+  body {
+    font-family: 'Poppins', sans-serif;
+  }
+  div {
+    font-family: 'Poppins', sans-serif;
+  }
+
+`;
+
+export default GlobalStyles;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@fontsource/poppins/300.css';
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/poppins/700.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
