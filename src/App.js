5291ec241cffecffee101d56cc2bbec5830540a4
sschwarzenberg <sschwarzenberg@uc.cl>
2023-12-10 22:59:04 -0300

    Join pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Global } from '@emotion/react';
+import Navbar from './structure/Navbar';
+import Sidebar from './structure/Sidebar';
+import GlobalStyles from './GlobalStyles';
+import Dashboard from './pages/Dashboard';
+import Appointments from './pages/Appointments';
+import Professionals from './pages/Professionals';
+import Documents from './pages/Documents';
+import Professional from './pages/Professional/Professional';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Global styles={GlobalStyles} />
+      <div className="App">
+        <Navbar />
+        <div className="content-container">
+          <Sidebar />
+          <div className="content" >
+          <h1>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/appointments" element={<Appointments />} />
+              <Route path="/professionals" element={<Professionals />} />
+              <Route path="/documents" element={<Documents />} />
+              <Route path="/professional" element={<Professional />} />
+            </Routes>
+          </h1>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Documents.js
@@ -0,0 +1,11 @@
+
+
+const Documents = () => {
+    return (
+      <div className="home">
+          <p> Documents </p>
+      </div>
+    );
+  }
+   
+  export default Documents;
